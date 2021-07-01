a1cd56aca029e0c6718b78d69c91cf395a09c938
Kerem Üstün <keremz1609@gmail.com>
2021-07-01 17:41:17 +0200

    classes start

--- 0966759_0964028 (change to zip for delivery)/CDMS-SourceFiles/users.py
+++ 0966759_0964028 (change to zip for delivery)/CDMS-SourceFiles/users.py
@@ -4,9 +4,9 @@ def registerUser():
     registerType = ""
     while registerType == "":
         print("\nWhat type of user do you want to register?")
-        print("Enter 1 to register a system administrator")
+        print("Enter 1 to register a client")
         print("Enter 2 to register an advisor")
-        print("Enter 3 to register a client")
+        print("Enter 3 to register a system administrator")
         print("Enter 'x' to go back")
         registerType = input("Register type: ")
         
@@ -21,3 +21,19 @@ def registerUser():
         else:
             registerType = ""
             print("Wrong input, enter one of the 4 inputs below")
+
+
+#Person class
+class Person:
+    def __init__(self):
+
+
+#Client class : Person
+class Client(Person):
+    def __init__(self):
+
+#Advisor class : Person
+
+#System admin class : Advisor
+
+#Super admin class : System admin
